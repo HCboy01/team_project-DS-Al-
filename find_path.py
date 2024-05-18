36b644866d464e7b54150aa0156a8379c61a7c07
Hoseong Kim <8536048@gmail.com>
2024-05-18 19:25:32 +0900

    다익스트라 추가

--- find_path.py
+++ find_path.py
@@ -1,44 +1,66 @@
 import indexing_station
-from collections import deque # 이건 구현해야하긴함 일단 임포트
+from collections import deque
+import heapq
+import time
 
-def find_path(start, end) :
+def bfs_path(start, end):
     station_ls = indexing_station.station_ls()
-    path = {start : [[start], 0]}
+    path = {start: [[start], 0]}
 
-    BFS_ls = deque()
-    BFS_ls.append(start)
-    while (True) :
-        cur_st = BFS_ls.popleft() # 현재 역
-        cur_path = path[cur_st][0] # 현재 역까지 온 경로
-        time = path[cur_st][1]
+    bfs_queue = deque([start])
+    while bfs_queue:
+        cur_st = bfs_queue.popleft()
+        cur_path, cur_time = path[cur_st]
 
-        print("현재역 = {0}, 현재역까지 온 경로 = {1}, 오는데 걸린 시간 {2}".format(cur_st, cur_path, time)) # 테스트 코드
-        if cur_st == end :
-            return(cur_path, time)
+        if cur_st == end:
+            return cur_path, cur_time
+
+        for line in station_ls[cur_st]:
+            for next_st in line:
+                next_time = cur_time + 1
+                if cur_st in indexing_station.hwanseung:
+                    next_time += 4
+                if next_st in path:
+                    if next_time < path[next_st][1]:
+                        path[next_st] = [cur_path + [next_st], next_time]
+                        bfs_queue.append(next_st)
+                else:
+                    path[next_st] = [cur_path + [next_st], next_time]
+                    bfs_queue.append(next_st)
+    
+    return None, float('inf')
+
+def dijkstra_path(start, end):
+    station_ls = indexing_station.station_ls()
+    min_heap = [(0, start, [start])]  # (time, current station, path)
+    visited = set()
+
+    while min_heap:
+        cur_time, cur_st, cur_path = heapq.heappop(min_heap)
+
+        if cur_st in visited:
+            continue
+
+        visited.add(cur_st)
         
-        for i in range(len(station_ls[cur_st])) : # 다음역 찾기
-            temp = time
-            if (i != 0) : # 환승 일때 시간만 2분 추가
-                time += 2
-            for j in range(len(station_ls[cur_st][i])) : # 해당 호선의 다음 역에 대해서
-                next_st = station_ls[cur_st][i][j] # 다음 역 선택
-                if next_st in path : # 이미 간적이 있으면
-                    if time + 2 < path[next_st][1] : # 시간 비교해서 더 적은거 선택
-                        BFS_ls.append(next_st) # 지금이 더 적으면 스택에 추가
-                        next_path = cur_path + [next_st] # path에 경로 추가
-                        path[next_st] = [next_path, time + 2]
-                else : # 없으면 그냥 추가
-                    BFS_ls.append(next_st)
-                    next_path = cur_path + [next_st]
-                    path[next_st] = [next_path, time + 2]
-            time = temp
+        if cur_st == end:
+            return cur_path, cur_time
 
+        for line in station_ls[cur_st]:
+            for next_st in line:
+                next_time = cur_time + 1
+                if cur_st in indexing_station.hwanseung:
+                    next_time += 4
+                heapq.heappush(min_heap, (next_time, next_st, cur_path + [next_st]))
 
+    return None, float('inf')
 
-        
-
-
-
-#start, end = map(int, input().split())
-start, end = "도봉", "상계"
-print(find_path(start, end))
+start, end = "도봉", "마천"
+start_time = time.time()
+print("BFS Path:", bfs_path(start, end))
+end_time = time.time()
+print(f"BFS time: {end_time-start_time}")
+start_time = time.time()
+print("Dijkstra Path:", dijkstra_path(start, end))
+end_time = time.time()
+print(f"Dijkstra time: {end_time-start_time}")
