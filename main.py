74c46c5bebcc5fc2e250300fd3c6b76b42436fe6
jinwook <141473794+jinnwoook@users.noreply.github.com>
2024-05-19 21:35:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ def main() :
     print("BFS Path:", bfs.find_path(start, end))
     end_time = time.time()
     print(f"BFS time: {end_time-start_time}")
+    
     start_time = time.time()
     print("Dijkstra Path:", dijkstra.find_path(start, end))
     end_time = time.time()
     print(f"a_star time: {end_time-start_time}")
+    
+    start_time = time.time()
+    print("a_star Path:", a_star.find_path(start, end))
+    end_time = time.time()
+    print(f"a_star time: {end_time-start_time}")
+    
+    
     start_time = time.time()
     print("a_star Path:", a_star.find_path(start, end))
     end_time = time.time()
